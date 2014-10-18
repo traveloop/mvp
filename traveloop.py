bfe91f6763fe4744d29c47345bc56913ce627ec3
snim2 <mount.sarah@gmail.com>
2014-10-18 16:39:56 +0100

    FIXED: Relative addressing bug

--- traveloop.py
+++ traveloop.py
@@ -1,15 +1,11 @@
-from flask import Flask, request
-app = Flask(__name__, static_url_path='')
+from flask import Flask, request, url_for, redirect
+app = Flask(__name__)
+
 
 @app.route('/')
 def root():
-    return app.send_static_file('static/html/index.html')
+    return redirect(url_for('static', filename='html/index.html'))
 
-import os
-@app.route('/js/<path:path>')
-def static_proxy(path):
-    # send_static_file will guess the correct MIME type
-    return app.send_static_file(os.path.join('js', path))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
